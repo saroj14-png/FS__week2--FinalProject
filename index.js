9d65b151ffd14241b682da3f96bc6963f512109a
saroj14-png <sarojani.powar@gmail.com>
2024-07-11 14:24:27 -0700

    blob replaced with img

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+
+/*-----------------------------------------header hamburger icon--------------------------------*/
+function openMenu() {
+  document.body.classList += " menu--open";
+}
+function closeMenu() {
+  document.body.classList.remove("menu--open");
+}
+
+/***-------------------------- Review section----------------------------------------***/
 const reviews = document.querySelectorAll(".review");
 let currentReview = 0;
-
 function showReview(index) 
 {
   reviews.forEach((review) => review.classList.remove("active"));
   reviews[index].classList.add("active");
 }
-
 document.querySelector(".prev-btn").addEventListener("click", () => 
 {
   currentReview = (currentReview - 1 + reviews.length) % reviews.length;
@@ -17,10 +25,9 @@ document.querySelector(".next-btn").addEventListener("click", () => {
   currentReview = (currentReview + 1) % reviews.length;
   showReview(currentReview);
 });
-
 showReview(currentReview);
 
-/*drop down faq-question*/
+/*----------------------------------------drop down faq-question-------------------------*/
 const questions = document.querySelectorAll(".dropbtn");
 const answers = document.querySelectorAll(".dropdown-content");
 
@@ -29,9 +36,7 @@ questions.forEach((question, index) => {
     answers[index].classList.toggle("active");
   });
 });
-
 var dropButtons = document.querySelectorAll(".dropbtn");
-
 // Add click event listener to each drop button
 dropButtons.forEach(function (dropButton) {
   dropButton.addEventListener("click", function () {
@@ -45,9 +50,3 @@ dropButtons.forEach(function (dropButton) {
   });
 });
 
-function openMenu() {
-  document.body.classList += " menu--open";
-}
-function closeMenu() {
-  document.body.classList.remove("menu--open");
-}
