e783e0668c0ccaa9374ff65ff3aa147b02b57f34
saroj14-png <sarojani.powar@gmail.com>
2024-07-19 21:23:51 -0700

    js code change reviews

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*-----------------------------------------header hamburger icon--------------------------------*/
 function openMenu() {
   document.body.classList += " menu--open";
@@ -8,25 +7,24 @@ function closeMenu() {
 }
 
 /***-------------------------- Review section----------------------------------------***/
-const reviews = document.querySelectorAll(".review");
-let currentReview = 0;
-function showReview(index) 
-{
-  reviews.forEach((review) => review.classList.remove("active"));
-  reviews[index].classList.add("active");
-}
-document.querySelector(".prev-btn").addEventListener("click", () => 
-{
-  currentReview = (currentReview - 1 + reviews.length) % reviews.length;
+document.addEventListener("DOMContentLoaded", function () {
+  const reviews = document.querySelectorAll(".review");
+  let currentReview = 0;
+  function showReview(index) {
+    reviews.forEach((review) => review.classList.remove("active"));
+    reviews[index].classList.add("active");
+  }
+  document.querySelector(".prev-btn").addEventListener("click", () => {
+    currentReview = (currentReview - 1 + reviews.length) % reviews.length;
+    showReview(currentReview);
+  });
+
+  document.querySelector(".next-btn").addEventListener("click", () => {
+    currentReview = (currentReview + 1) % reviews.length;
+    showReview(currentReview);
+  });
   showReview(currentReview);
 });
-
-document.querySelector(".next-btn").addEventListener("click", () => {
-  currentReview = (currentReview + 1) % reviews.length;
-  showReview(currentReview);
-});
-showReview(currentReview);
-
 /*----------------------------------------drop down faq-question-------------------------*/
 const questions = document.querySelectorAll(".dropbtn");
 const answers = document.querySelectorAll(".dropdown-content");
@@ -49,4 +47,3 @@ dropButtons.forEach(function (dropButton) {
     dropdownContent.classList.toggle("show");
   });
 });
-
